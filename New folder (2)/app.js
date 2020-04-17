60c5488f9eea7e2b8be690a756fcee325cff7744
fs-s <55434294+fs-s@users.noreply.github.com>
2020-04-17 15:28:02 +0300

    Update app.js

--- New folder (2)/app.js
+++ New folder (2)/app.js
@@ -2,7 +2,7 @@ const textboxdiv= document.getElementById('text-box')
 const typingArea = document.getElementById('typing-area')
 const feedbackdiv = document.getElementById('feedback')
 
-const text= 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+const text= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et semper enim. Phasellus dictum, felis vel porttitor ornare, odio est tincidunt turpis, vitae vehicula felis dolor a tellus. Nulla facilisi. Morbi malesuada purus eu erat ullamcorper, convallis bibendum enim finibus. Suspendisse egestas tellus justo, sit amet euismod dui consequat sit amet. Fusce consectetur vehicula ante at interdum. Nunc nunc ex, laoreet et auctor quis, blandit in urna. Vestibulum ut tristique nisl, nec ultrices elit.'
 textboxdiv.innerText = text
 let wordCount = 0
 highlight()
@@ -14,15 +14,18 @@ typingArea.focus()
 typingArea.addEventListener('keydown', event => {
 
 if (event.keyCode == 32) {
-    if (text.split(' ')[wordCount] != typingArea.Value.split(' ')[wordCount]){
-        //console.log('error!')
+    console.log(typingArea.value)
+    if( text.split(' ')[wordCount] != typingArea.value.split(' ')[wordCount]){
         errorCount++
         feedbackdiv.innerText = errorCount
     }
+
+
+
     wordCount++
     highlight()
-    
 }
+
 
 })
 
@@ -30,5 +33,5 @@ function highlight() {
     let wordsArray = text.split(' ')
     wordsArray[wordCount] = '<span class=highlight>' + wordsArray[wordCount]  + '</span>'
     textboxdiv.innerHTML = wordsArray.join(' ')
-    
+
 }
